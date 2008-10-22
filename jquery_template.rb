dafd2ce54cbc5ef7f0fdffbed1785332336ec461
collin <collintmiller@gmail.com>
2008-10-22 03:14:05 -0500

    and a little bit of ruby to help out

---
+++ jquery_template.rb
@@ -0,0 +1,73 @@
+module JQuery
+  module Template
+    class NoTemplateError < StandardError; end
+    class TooManyTranslationsError < StandardError; end
+    class TranslationUnspicifiedError < StandardError; end
+    class OnlySupplyObjectOrCollectionError < StandardError; end
+    class InvalidOptionError < StandardError; end
+    
+    def render options={}
+      Renderer.new(options).render
+    end
+    
+    class Renderer
+      def self.property property_id
+        send :define_method, "#{property_id}?" do
+          @options.include? property_id
+        end
+        
+        send :define_method, property_id do
+          @options[property_id]
+        end
+      end
+      
+      property :property
+      property :template
+      property :object
+      property :collection
+      property :translate
+      
+      def initialize options
+        @options = options
+        raise InvalidOptionError unless options_valid?
+      end
+      
+      def options_valid?
+        @options.each do |option, value|
+          return false unless respond_to? option
+        end
+      end
+      
+      def render
+        if property?
+          "\#{#{property}}"
+        elsif template? and only_template
+          "={#{template}}"
+        elsif object? or collection?
+          raise NoTemplateError unless template?
+          raise OnlySupplyObjectOrCollectionError if object? and collection?
+          "={#{template}:#{object or collection}}"
+        elsif translate?
+          raise NoTemplateError unless template?
+          "=[#{template}||#{translation}]"
+        end
+      end
+      
+      def translation
+        name, list_name = *translation_options
+        ""
+      end
+      
+      def translation_options
+        opts = translate.to_a
+        raise TooManyTranslationsError if opts.length > 1
+        raise TranslationUnspicifiedError if opts.length == 0
+        return opts
+      end
+      
+      def only_template
+        not(object? || collection? || translate?)
+      end
+    end
+  end
+end
---
+++ jquery_template_spec.rb
@@ -0,0 +1,49 @@
+require 'jquery_template'
+
+describe JQuery::Template do
+  include JQuery::Template
+  
+  it "renders" do
+    render(:property => :awesomeness).should == '#{awesomeness}'
+  end
+  
+  it "renders templates" do
+    render(:template => :layout).should == "={layout}"
+  end
+  
+  it "renders templates with an object" do
+    render(:template => :bookshelf, :object => :book).should == "={bookshelf:book}"
+  end
+  
+  it "renders templates with a collection" do
+    render(:template => :bookshelf, :collection => :books).should == "={bookshelf:books}"
+  end
+  
+  it "renders list expression syntax" do
+    render(:template => :storefront, :translate => {:buyers => :customer_name})
+  end
+  
+  it "requires template with object" do
+    lambda {
+      render :object => :whatever
+    }.should raise_error
+  end
+  
+  it "requires template with collection" do
+    lambda {
+      render :collection => :whatevers
+    }.should raise_error
+  end
+  
+  it "only allows one object or collection" do
+    lambda {
+      render :object => :whatever, :collection => :whatevers, :template => :any
+    }.should raise_error
+  end
+  
+  it "errors against invalid properties" do
+    lambda {
+      render :property => :real, :dumb_property => :fake
+    }.should raise_error 
+  end
+end
